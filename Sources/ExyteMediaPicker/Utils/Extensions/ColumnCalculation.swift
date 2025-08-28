dd9d41c2e39cf9cef18b0bd508a71ada4570051b
Fred Bowker <fred.bowker@locovo.co>
2025-08-28 11:21:52 +0100

    changed to have fixed 3 column width instead of dynamic width

--- Sources/ExyteMediaPicker/Utils/Extensions/ColumnCalculation.swift
+++ Sources/ExyteMediaPicker/Utils/Extensions/ColumnCalculation.swift
@@ -10,8 +10,7 @@ import SwiftUI
 @MainActor
 func calculateColumnWidth(spacing: CGFloat) -> (CGFloat, [GridItem]) {
     let gridWidth = UIScreen.main.bounds.width
-    let minColumnWidth = 100.0
-    let wholeCount = CGFloat(Int(gridWidth / minColumnWidth))
+    let wholeCount = 3.0
     let noSpaces = gridWidth - spacing * (wholeCount - 1)
     let columnWidth = noSpaces / wholeCount
     let columns = Array(repeating: GridItem(.fixed(columnWidth), spacing: spacing, alignment: .top), count: Int(wholeCount))
